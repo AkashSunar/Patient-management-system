7ecf9c91206f058d94f841bb2808f4f69091cb12
akash sunar <sunarakash1122@gmail.com>
2023-12-27 15:21:05 +0545

    feat:login completed using token

--- patient-management-backend/app.js
+++ patient-management-backend/app.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
-const userControllers = require("./controllers/users")
-const patientControllers=require("./controllers/patients")
+const express = require("express");
+const app = express();
+const userControllers = require("./controllers/users");
+const patientControllers = require("./controllers/patients");
+
 const { errorHandler, unknownEndpoint } = require("./utils/middleware");
-const mongoose = require('mongoose');
-const { url } = require("./utils/config"); 
-mongoose.set('strictQuery', false);
+const mongoose = require("mongoose");
+const { url } = require("./utils/config");
+mongoose.set("strictQuery", false);
 mongoose.connect(url);
-app.use(express.json())
-app.use("/api/user", userControllers)
-app.use("/api/patient",patientControllers)
+app.use(express.json());
+app.use("/api/user", userControllers);
+app.use("/api/patient", patientControllers);
+
 app.use(unknownEndpoint);
 module.exports = app;
